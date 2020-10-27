5243bfa80a3ff72f0d3ac3b1a9dc0ab93aa4491b
= <=>
2020-10-27 11:49:01 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+function getDogImage(imageNumber) {
+  debugger
+  let breed = $('#breed').val();
+  fetch(`https://dog.ceo/api/breed/${breed}/images/random/${imageNumber}`) 
+  // adding to the end of the URL says how many numbers are added and then pass the argument into the function
+  .then(response => response.json()) 
+  .then(responseJson => displayResults(responseJson))
+  .catch(error => alert('Something went wrong! Try again later!'));
+}
+
+function displayResults(responseJson){
+  let dogs = [];
+  for(let i = 0; i < responseJson.message.length; i++){
+    dogs.push(`<img src='${responseJson.message[i]}' class='results-image'>`);
+  }
+  $('.results-img').html(dogs);
+}
+
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    let imageNumber = $('#getDog').val();
+    
+    
+    if(imageNumber < 3) {
+      imageNumber = 3;
+    }
+    getDogImage(imageNumber);
+
+  });
+}
+
+
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
